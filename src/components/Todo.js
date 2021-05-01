845f885a74315c948304102c61f3c699ed5b8a9a
Caio Serra <caio_mello@id.uff.br>
2021-05-01 20:34:40 -0300

    feat add event handler to delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,23 @@
 // name of the function should start with a capital character to differentiate from vanilla html tags
 function Todo(props) {
+  // function nesting (function inside function) is a common javascript feature which is used here
+  function deleteHandler() {
+    // since the funcion is nested, we can use the same parameteres as the parent function
+    // console.log(props.text)
+  }
+
   return (
     <div className="card">
-      {/*we use {} to specify that it is a dynamic code, not plain html (JSX)*/}
+      {/* We use {} to specify that it is a dynamic code, not plain html (JSX)*/}
       <h2>{props.text}</h2>
-      {/*class attribute name is className since this is a javascript object (JSX), NOT html*/}
+      {/* Class attribute name is className since this is a javascript object (JSX), NOT html*/}
       <div className="actions">
-        <button className="btn">Delete</button>
+        {/* Event listener should be passed as function pointers instead of function calls
+         * (whitout parenthesis()) since we want react to decide when to call the function (onClick)
+         * If we make a function call, it will be executed every time the view is rendered */}
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
