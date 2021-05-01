a6dfb4d31ea782ec5778ceb56f24629b255d8334
Caio Serra <caio_mello@id.uff.br>
2021-05-01 19:16:51 -0300

    adds basic card view + styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react"
+import React from "react";
 
 function App() {
   return (
-    <div>Hello World React!</div>
+    <div>
+      <h1>My Todos</h1>
+      <div className='card'> {/*this is className since this is a javascript object (JSX), NOT html*/}
+        <h2 className='actions'>My title</h2>
+        <div>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
