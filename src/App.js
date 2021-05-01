3731134cebc75f18ba46491eefc1767d42e9635b
Caio Serra <caio_mello@id.uff.br>
2021-05-01 19:17:45 -0300

    fix card title styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div>
       <h1>My Todos</h1>
       <div className='card'> {/*this is className since this is a javascript object (JSX), NOT html*/}
-        <h2 className='actions'>My title</h2>
-        <div>
+        <h2 >My title</h2>
+        <div className='actions'>
           <button className='btn'>Delete</button>
         </div>
       </div>
