42c430fe0a02ab13532c549f9b966d7087a223e9
Caio Serra <caio_mello@id.uff.br>
2021-05-04 12:48:12 -0300

    fix add cancel and confirm handler to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,23 @@
 function Modal(props) {
+
+  function cancelHandler(){
+    props.onCancel()
+  }
+
+  function confirmHandler(){
+    props.onConfirm()
+  }
+
   return (
     <div className="modal">
       <p>Are you sure?</p>
       {/* props can also be functions chaining events.
        *This is interesting because you can have all your state centered in one component and just make some parts of it avaliable
        * to other components by function, protecting and making it easier to maintain */}
-      <button className="btn btn--alt" onClick={props.onCancel}>
+      <button className="btn btn--alt" onClick={cancelHandler}>
         Cancel
       </button>
-      <button className="btn" onClick={props.onConfirm}>
+      <button className="btn" onClick={confirmHandler}>
         Confirm
       </button>
     </div>
