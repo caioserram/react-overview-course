16d27b496015d8c9aae5bdd0fc1b9c1ff98afe44
Caio Serra <caio_mello@id.uff.br>
2021-05-04 13:21:03 -0300

    :routing-setup add page by page routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react"
-import {Route} from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
     <div>
-      <Route path='/'>
-        
-      </Route>
+      <Switch>
+        {/* By default, react renders all components that a path matches. Example if a path is "/" 
+      and another is "/favorites", those two components will be nested.
+      We add Switch element and exact property (defaults to true) so it won't be nested*/}
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
